576ec016fe0cd33b51c24ed7dfdbc5393571991a
version-1 <j.onodera26@gmail.com>
2020-02-18 11:53:21 -0800

    add javadoc

--- src/lab1/classes/Model.java
+++ src/lab1/classes/Model.java
@@ -1,5 +1,8 @@
 package lab1.classes;
 
+/**
+ * @author Jiro, Rodrigo
+ */
 public class Model {
   private String firstName;
   private String lastName;
@@ -18,9 +21,21 @@ public class Model {
   public static final int SMOKER_DEDUCTION_DOLLARS_PER_HOUR = 10;
 
 
+  /**
+   *
+   */
   public Model() {
   }
 
+  /**
+   *
+   * @param firstName
+   * @param lastName
+   * @param height
+   * @param weight
+   * @param canTravel
+   * @param smokes
+   */
   public Model(String firstName, String lastName, int height, double weight, boolean canTravel, boolean smokes) {
     setFirstName(firstName);
     setLastName(lastName);
@@ -30,6 +45,13 @@ public class Model {
     setSmokes(smokes);
   }
 
+  /**
+   *
+   * @param firstName
+   * @param lastName
+   * @param height
+   * @param weight
+   */
   public Model(String firstName, String lastName, int height, double weight) {
     setFirstName(firstName);
     setLastName(lastName);
@@ -39,41 +61,69 @@ public class Model {
     setSmokes(false);
   }
 
+  /**
+   * @return firstName
+   */
   public String getFirstName() {
     return this.firstName;
   }
 
+  /**
+   * @return lastName
+   */
   public String getLastName() {
     return this.lastName;
   }
 
+  /**
+   * @return height
+   */
   public int getHeight() {
     return this.height;
   }
 
+  /**
+   * @return weight in inches
+   */
   public double getWeight() {
     return this.weight;
   }
 
+  /**
+   * @return  whether model can travel
+   */
   public boolean getCanTravel() {
     return this.canTravel;
   }
 
+  /**
+   * @return  whether model smokes
+   */
   public boolean getSmokes() {
     return this.smokes;
   }
 
+  /**
+   * @return  display string of height and inches
+   */
   public String getHeightInFeetAndInches() {
     final int feet = this.height / INCHES_PER_FOOT;
     final int inches = this.height % INCHES_PER_FOOT;
     return feet + " feet " + inches + " inches";
   }
 
+  /**
+   * @return  rounded weight in kg
+   */
   public long getWeightKg() {
     return Math.round((long) (this.weight / POUNDS_PER_KG));
   }
 
 
+  /**
+   * @param  firstName
+   * @return void
+   */
   public void setFirstName(String firstName) {
     if (firstName.length() < 2 && firstName.length() > 20 ) {
       return;
@@ -81,6 +131,10 @@ public class Model {
     this.firstName = firstName;
   }
 
+  /**
+   * @param  lastName
+   * @return void
+   */
   public void setLastName(String lastName) {
     if (lastName.length() < 2 && lastName.length() > 20 ) {
       return;
@@ -88,14 +142,26 @@ public class Model {
     this.lastName = lastName;
   }
 
+  /**
+   * @param  canTravel
+   * @return void
+   */
   public void setCanTravel(boolean canTravel) {
     this.canTravel = canTravel;
   }
 
+  /**
+   * @param  smokes
+   * @return void
+   */
   public void setSmokes(boolean smokes) {
     this.smokes = smokes;
   }
 
+  /**
+   * @param  pound
+   * @return void
+   */
   public void setWeight(double pound) {
     if (pound < 80 && pound > 280) {
       return;
@@ -103,6 +169,10 @@ public class Model {
     this.weight = pound;
   }
 
+  /**
+   * @param  weight in killograms
+   * @return void
+   */
   public void setWeight(long kilograms) {
     double pound = kilograms * POUNDS_PER_KG;
     if (pound < 80 && pound > 280) {
@@ -111,6 +181,10 @@ public class Model {
     this.weight = pound;
   }
 
+  /**
+   * @param  height in inches
+   * @return void
+   */
   public void setHeight(int inches) {
     if (inches < 24 && inches > 84) {
       return;
@@ -118,6 +192,11 @@ public class Model {
     this.height = inches;
   }
 
+  /**
+   * @param  height in feets
+   * @param  height in inches
+   * @return void
+   */
   public void setHeight(int feets, int inches) {
     int height = feets * INCHES_PER_FOOT + inches;
     if (height < 24 && height > 84) {
@@ -126,6 +205,10 @@ public class Model {
     this.height = height;
   }
 
+  /**
+   * print details of a model
+   * @return void
+   */
   public void printDetails() {
     String travel = getCanTravel() ? "Does travel" : "Does not travel";
     String smokes = getSmokes() ? "Does smokes" : "Does not smokes";
@@ -138,6 +221,10 @@ public class Model {
     );
   }
 
+  /**
+   * calculate pay per hour
+   * @return pay dollar per hour
+   */
   public int calculatePayDollarsPerHour () {
     int salary = BASE_RATE_DOLLARS_PER_HOUR;
     if (getHeight() > TALL_INCHES && getWeight() < THIN_POUNDS) {
@@ -152,6 +239,10 @@ public class Model {
     return salary;
   }
 
+  /**
+   * display model details
+   * @return void
+   */
   public void displayModelDetails () {
     String travels = getCanTravel() ? "yep" : "nope";
     String smokes = getSmokes() ? "yep" : "nope";
